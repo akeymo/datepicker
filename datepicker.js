f15d88fc552158e0d0956754504b4d66ba75a464
周闽 <min.zhou@zhoumindeMacBook-Air.local>
2017-07-29 10:35:04 +0800

    完成数据渲染

--- datepicker.js
+++ datepicker.js
@@ -1,6 +1,14 @@
 (function(){
 	var datepicker = {
+
+		init: function($dom){
+			var _html = this.buildUi();
+			var ele = document.querySelector($dom);
+			ele.innerHTML = _html;
+		},
+
 		getMonthData: function(year, month){
+			// 获取月份数据
 			var ret = [];
 
 			if(!year || !month){
@@ -11,6 +19,10 @@
 			}
 
 			var firstDay = new Date(year, month-1, 1);
+
+			year = firstDay.getFullYear();
+			month = firstDay.getMonth() + 1;
+
 			var firstDayWeekDay = firstDay.getDay();
 			if(firstDayWeekDay === 0){
 				firstDayWeekDay = 7;
@@ -47,8 +59,55 @@
 				})
 			}
 
-			return ret;
+			return {
+				year: year,
+				month: month,
+				dates: ret
+			};
 		},
+
+		buildUi: function(year, month){
+			// 生成日历html结构
+			var montnData = this.getMonthData(year,month);
+
+			var html = '<div class="ui-datepicker-header">' 
+					 	+ '<a href="#" class="ui-datepicker-btn ui-datepicker-prev-btn">&lt;</a>' 
+						+ '<a href="#" class="ui-datepicker-btn ui-datepicker-next-btn">&gt;</a>' 
+						+ '<span class="ui-datepicker-curr-month">' + montnData.year + '-' + montnData.month +'</span>' 
+						+ '</div>' 
+						+ '<div class="ui-datepicker-body">' 
+						+ '<table>' 
+						+ '<thead>'
+						+ '<tr>'
+						+ '<th>一</th>'
+						+ '<th>二</th>'
+						+ '<th>三</th>'
+						+ '<th>四</th>'
+						+ '<th>五</th>'
+						+ '<th>六</th>'
+						+ '<th>日</th>'
+						+ '</tr>'
+						+ '</thead>'
+						+ '<tbody>';
+
+			for(var i=0, len=montnData.dates.length; i<len; i++){
+				if(i%7 === 0){
+					// 每星期的第一天
+					html += '<tr>'
+				}
+
+				html += '<td>' + montnData.dates[i].showDate + '</td>'
+
+				if(i%7 === 6){
+					// 每星期最后一天
+					html += '</tr>'
+				}
+			}	
+
+			html += '</tbody> </table> </div>';
+
+			return html;
+		}
 
 
 	};
